54c3eb403dc5dbf4cad77e9ea11b3ece223c7f4e
Kartik2778 <118062207+Kartik2778@users.noreply.github.com>
2023-11-04 19:57:35 +0530

    Add files via upload

---
+++ BigInteger.h
@@ -0,0 +1,8 @@
+struct node;
+struct node* add(struct node* ,struct node* );
+struct node* subtract(struct node* ,struct node* );
+struct node* multiply(long long int,long long int);
+struct node* divide(long long int,long long int);
+struct node* modulus(long long int,long long int);
+void addNode(struct node **, int );
+void display(struct node *);
